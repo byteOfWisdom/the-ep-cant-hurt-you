89cb2bfef99f367c455597054510291d7d04fe7b
Leoni <leoni.dessau@mailbox.org>
2025-08-29 15:15:30 +0200

    we even have errors

--- scripts/amplifications.py
+++ scripts/amplifications.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sys import argv
 from matplotlib import pyplot as plt
+from labtools.perror import ev, value, error
 
 def load_csv(fname):
     with open(fname, "r") as file:
@@ -36,13 +37,15 @@ def get_SI_values(fname):
     unit, unit_value = find_unit(tstep)
     amplitude = data * vstep * cal_factor
     scaled_time = time * unit_value
-    return scaled_time, amplitude
+    dt = scaled_time[1] - scaled_time[0]
+    dv = vstep * cal_factor
+    return scaled_time, amplitude, dt, dv
 
 
 def get_freq(times, values):
     fft_values = np.abs(np.fft.fft(values))
     freqs = np.fft.fftfreq(values.size, d = times[1] - times[0])
-    return abs(freqs[fft_values == max(fft_values)])[0]
+    return ev(abs(freqs[fft_values == max(fft_values)])[0], freqs[1] - freqs[0])
 
 
 def get_Upp(voltages):
@@ -51,10 +54,10 @@ def get_Upp(voltages):
 
 
 def extract_data(fname1, fname2):
-    t1, u1 = get_SI_values(fname1)
-    t2, u2 = get_SI_values(fname2)
+    t1, u1, dt1, dv1 = get_SI_values(fname1)
+    t2, u2, dt2, dv2 = get_SI_values(fname2)
     f = 0.5 * (get_freq(t1, u1) + get_freq(t2, u2))
-    v = get_Upp(u2) / get_Upp(u1)
+    v = ev(get_Upp(u2), dv2) / ev(get_Upp(u1), dv1)
     return f, v
 
 
@@ -70,17 +73,21 @@ def collate(start, count):
     start_num = int(start[-4:])
 
     amps = []
+    damps = []
     freqs = []
+    dfreqs = []
 
     for i in range(start_num, start_num + count):
         f_ch1 = path + pad(i) + f"/A{pad(i)}CH1.CSV"
         f_ch2 = path + pad(i) + f"/A{pad(i)}CH2.CSV"
 
         f, v = extract_data(f_ch1, f_ch2)
-        freqs.append(f)
-        amps.append(v)
+        freqs.append(f.value)
+        dfreqs.append(f.error)
+        amps.append(v.value)
+        damps.append(v.error)
 
-    plt.plot(freqs, amps)
+    plt.errorbar(freqs, amps, fmt=".", yerr=damps, xerr=dfreqs)
 
 
 if __name__ == "__main__":
@@ -94,6 +101,6 @@ if __name__ == "__main__":
     plt.grid(which="major")
     plt.grid(which="minor", linestyle=":", linewidth=0.5)
     plt.gca().minorticks_on()
-    plt.legend()
+    #plt.legend()
     plt.loglog()
     plt.show()
